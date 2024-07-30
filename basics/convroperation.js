d946f44ddd46ea9c9212b4c19031f6212272bd34
Saif Ali <86141025+Ccybertron@users.noreply.github.com>
2024-07-30 10:19:11 +0000

    Conversion and operation

---
+++ basics/convroperation.js
@@ -0,0 +1,29 @@
+// let score = "50ac"
+// let score = 50
+// let score = null
+let score = undefined
+// let score = true
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number (score)
+console.log (typeof valueInNumber )
+console.log(valueInNumber)
+
+// "50" => 50
+// "50ac" =>NaN
+// true =>1 , False =>0
+ 
+let islogIn = 1
+let booleanLogIn = Boolean(islogIn)
+console.log(booleanLogIn)
+
+// 1 => true , 0 =>false
+// "" empty => false
+// "anything" =>true
+
+//converting number to string
+let nmbr = 50
+let StringNmbr = String(nmbr)
+console.log(StringNmbr)
+console.log(typeof StringNmbr)
