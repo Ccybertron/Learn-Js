45d508688a0f39a584383d1d23a25a4abff46e80
Saif Ali <86141025+Ccybertron@users.noreply.github.com>
2024-07-30 11:08:25 +0000

    conversion and operation

--- basics/convroperation.js
+++ basics/convroperation.js
@@ -1,29 +1,44 @@
-// let score = "50ac"
-// let score = 50
-// let score = null
-let score = undefined
-// let score = true
-console.log(typeof score)
-console.log(typeof (score))
+// // let score = "50ac"
+// // let score = 50
+// // let score = null
+// let score = undefined
+// // let score = true
+// // console.log(typeof score)
+// // console.log(typeof (score))
 
-let valueInNumber = Number (score)
-console.log (typeof valueInNumber )
-console.log(valueInNumber)
+// let valueInNumber = Number (score)
+// // console.log (typeof valueInNumber )
+// // console.log(valueInNumber)
 
-// "50" => 50
-// "50ac" =>NaN
-// true =>1 , False =>0
+// // "50" => 50
+// // "50ac" =>NaN
+// // true =>1 , False =>0
  
-let islogIn = 1
-let booleanLogIn = Boolean(islogIn)
-console.log(booleanLogIn)
+// let islogIn = 1
+// let booleanLogIn = Boolean(islogIn)
+// // console.log(booleanLogIn)
 
-// 1 => true , 0 =>false
-// "" empty => false
-// "anything" =>true
+// // 1 => true , 0 =>false
+// // "" empty => false
+// // "anything" =>true
 
-//converting number to string
-let nmbr = 50
-let StringNmbr = String(nmbr)
-console.log(StringNmbr)
-console.log(typeof StringNmbr)
+// //converting number to string
+// let nmbr = 50
+// let StringNmbr = String(nmbr)
+// // console.log(StringNmbr)
+// // console.log(typeof StringNmbr)
+
+// Operations //
+let str1 = "hel"
+let str2 = "lo"
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log(1 + "2") 
+console.log("1" + 2) // priority to first term ie if its string treat all as string
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+let counter = 100;
+ counter++;
+ console.log(counter)
