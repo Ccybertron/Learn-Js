95a50edf8f90b34de487cdbd50c62bb86a861a4f
Saif Ali <86141025+Ccybertron@users.noreply.github.com>
2024-07-30 06:37:35 +0000

    variables

---
+++ basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 12354
+let accountEmail ="cybertron@gmail.com"
+var accountPassword = "1234456"
+accountCity ="swat"
+let accountState;
+// accountId = 2
+console.log(accountId);
+
+accountEmail = "abc@gmail.com"
+accountPassword = " 7658"
+accountCity = "peshawar"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,]);
