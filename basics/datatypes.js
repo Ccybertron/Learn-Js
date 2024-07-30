e956e65c9396c76503a8b0cec284c1a61289ed33
Saif Ali <86141025+Ccybertron@users.noreply.github.com>
2024-07-30 09:47:18 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,11 @@
+let name = "cybertron"  //string
+let age = 20  // number
+let isLogin = false/true // boolean
+// some other data types in js
+
+//bigint
+//null => object
+//undefined 
+//symbol => represent unique value
+
+console.log(typeof null)
