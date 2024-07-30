506c0c995aa1d8655aa2d1677f82e71587f9e64c
Saif Ali <86141025+Ccybertron@users.noreply.github.com>
2024-07-30 17:09:52 +0500

    Add files via upload

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,47 @@
+// Primitive Data Types
+
+// Number
+console.log(42);        
+console.log(3.14);      
+
+// String
+console.log("hello");   
+console.log('world');   
+
+// Boolean
+console.log(true);      
+console.log(false);     
+
+// Undefined
+let x;
+
+// Null
+let y = null;
+
+// Symbol 
+const id = Symbol('546');
+const anotherId = Symbol('546')  ;
+
+console.log(id === anotherId)  
+
+const bigNumber = 4623643276401646
+ 
+Non-Primitive Data Types (Arrays , objects , Funtion)
+
+const games = ["cricket", " football" , "hockey"]
+console.log(games)
+
+let myObj = {
+    name: "cybertron",
+    age: 12,
+    address: "peshawar"
+}
+ 
+const myfunction = function() {
+    console.log("hello world")
+    console.log(myfunction)
+}
+
+
+
+
